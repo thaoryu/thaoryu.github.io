c9257fa0ad4533cb290c4f24600db9511cbd8ab6
thaoryu <fthao@byui.edu>
2019-06-05 16:54:11 -0600

    tutor

---
+++ assignments/lesson-7/js/currentdate.js
@@ -0,0 +1,9 @@
+var d = new Date();
+var daylist = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+var weekday = daylist[d.getDay()];
+var day = d.getDate();
+var monthlist = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+var monthname = monthlist[d.getMonth()];
+var year = d.getFullYear();
+var fulldate = weekday + ", " + day + " " + monthname + " " + year;
+document.getElementById("myDate").innerHTML = fulldate;
